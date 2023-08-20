22954d976a4048c8d0fe621246dd01b1fdae68af
Luis Acuña <luisandresverdugox@gmail.com>
2023-08-20 14:51:54 -0500

    Subiendo estructura del proyecto

---
+++ bootcoin/src/main/java/api/bootcoin/BootcoinApplication.java
@@ -0,0 +1,13 @@
+package api.bootcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcoinApplication.class, args);
+	}
+
+}
